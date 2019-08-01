aee32f3c3a75b83c637fe56d742136e8d40968a1
anson <abddd12444@gmail.com>
2019-08-01 13:54:25 +0800

    Update utils Add sample code

--- utils.py
+++ utils.py
@@ -17,71 +17,37 @@ class LinearRegression:
         if categorical is not None:
             self.X = self.X.drop(categorical, axis = 1)
             for cat in categorical:
-                TEMP = [ ]
-                for i in range(len(self.df)):
-                    temp = [ ]
-                    values = self.df[cat].unique()
-                    if ascending is False:
-                        values = values[::-1]
-                    for j in range(len(values) - 1):
-                        if self.df[cat].loc[i] == values[j]:
-                            temp.append(1)
-                        else:
-                            temp.append(0)
-                    TEMP.append(temp)
-                TEMP = np.array(TEMP)
                 values = self.df[cat].unique()
                 if ascending is False:
                     values = values[::-1]
                 for i in range(len(values) - 1):
-                    col = cat + '_' + str(values[i])
-                    self.X[col] = TEMP.transpose()[i]
+                    temp = [ ]
+                    for j in range(len(self.df)):
+                        if self.df[cat].loc[j] == values[i]:
+                            temp.append(1)
+                        else:
+                            temp.append(0)
+                    col = cat + str(values[i])
+                    self.X[col] = temp
 
         if interaction is not None:
             for inter in interaction:
-                TEMP = [ ]
-                for i in range(len(self.df)):
-                    temp = [ ]
-                    if inter[0] in categorical and inter[1] in categorical:
-                        col_1 = self.df[inter[0]].unique()
-                        col_2 = self.df[inter[1]].unique()
-                        if ascending is False:
-                            col_1 = col_1[::-1]
-                            col_2 = col_2[::-1]
-                        for j in range(len(col_1) - 1):
-                            for k in range(len(col_2) - 1):
-                                if self.df[inter[0]].loc[i] == col_1[j] and self.df[inter[1]].loc[i] == col_2[k]:
-                                    temp.append(1)
-                                else:
-                                    temp.append(0)
-                    else:
-                        if inter[0] in categorical and inter[1] not in categorical:
-                            cat = inter[0]
-                            con = inter[1]
-                        else:
-                            cat = inter[1]
-                            con = inter[0]
-                        col = self.df[cat].unique()
-                        if ascending is False:
-                            col = col[::-1]
-                        for j in range(len(col) - 1):
-                            if self.df[cat].loc[i] == col[j]:
-                                temp.append(self.df[con].loc[i])
-                            else:
-                                temp.append(0)
-
-                    TEMP.append(temp)
-                TEMP = np.array(TEMP)
                 if inter[0] in categorical and inter[1] in categorical:
                     col_1 = self.df[inter[0]].unique()
                     col_2 = self.df[inter[1]].unique()
                     if ascending is False:
                         col_1 = col_1[::-1]
                         col_2 = col_2[::-1]
-                    for j in range(len(self.df[inter[0]].unique()) - 1):
-                        for k in range(len(self.df[inter[1]].unique()) - 1):
-                            col = inter[0] + '_' + str(col_1[j]) + '_' + inter[1] + '_' + str(col_2[k])
-                            self.X[col] = TEMP.transpose()[j * (len(col_2) - 1) + k]
+                        for i in range(len(col_1) - 1):
+                            for j in range(len(col_2) - 1):
+                                temp = [ ]
+                                for k in range(len(self.df)):
+                                    if self.df[inter[0]].loc[k] == col_1[i] and self.df[inter[1]].loc[k] == col_2[j]:
+                                        temp.append(1)
+                                    else:
+                                        temp.append(0)
+                                col = inter[0] + str(col_1[i]) + '_' + inter[1] + str(col_2[j])
+                                self.X[col] = temp
                 else:
                     if inter[0] in categorical and inter[1] not in categorical:
                         cat = inter[0]
@@ -89,12 +55,78 @@ class LinearRegression:
                     else:
                         cat = inter[1]
                         con = inter[0]
-                    col = self.df[cat].unique()
+                    cols = self.df[cat].unique()
                     if ascending is False:
-                        col = col[::-1]
-                    for j in range(len(col) - 1):
-                        col = cat + '_' + str(col[j]) + '_' + con
-                        self.X[col] = TEMP.transpose()[j]
+                        cols = cols[::-1]
+                    for i in range(len(cols) - 1):
+                        temp = [ ]
+                        for j in range(len(self.df)):
+                            if self.df[cat].loc[i] == cols[i]:
+                                temp.append(self.df[con].loc[i])
+                            else:
+                                temp.append(0)
+                        col = cat + str(cols[i]) + con
+                        self.df[col] = temp
+
+
+        # if interaction is not None:
+        #     for inter in interaction:
+        #         TEMP = [ ]
+        #         for i in range(len(self.df)):
+        #             temp = [ ]
+        #             if inter[0] in categorical and inter[1] in categorical:
+        #                 col_1 = self.df[inter[0]].unique()
+        #                 col_2 = self.df[inter[1]].unique()
+        #                 if ascending is False:
+        #                     col_1 = col_1[::-1]
+        #                     col_2 = col_2[::-1]
+        #                 for j in range(len(col_1) - 1):
+        #                     for k in range(len(col_2) - 1):
+        #                         if self.df[inter[0]].loc[i] == col_1[j] and self.df[inter[1]].loc[i] == col_2[k]:
+        #                             temp.append(1)
+        #                         else:
+        #                             temp.append(0)
+        #             else:
+        #                 if inter[0] in categorical and inter[1] not in categorical:
+        #                     cat = inter[0]
+        #                     con = inter[1]
+        #                 else:
+        #                     cat = inter[1]
+        #                     con = inter[0]
+        #                 col = self.df[cat].unique()
+        #                 if ascending is False:
+        #                     col = col[::-1]
+        #                 for j in range(len(col) - 1):
+        #                     if self.df[cat].loc[i] == col[j]:
+        #                         temp.append(self.df[con].loc[i])
+        #                     else:
+        #                         temp.append(0)
+        #
+        #             TEMP.append(temp)
+        #         TEMP = np.array(TEMP)
+        #         if inter[0] in categorical and inter[1] in categorical:
+        #             col_1 = self.df[inter[0]].unique()
+        #             col_2 = self.df[inter[1]].unique()
+        #             if ascending is False:
+        #                 col_1 = col_1[::-1]
+        #                 col_2 = col_2[::-1]
+        #             for j in range(len(self.df[inter[0]].unique()) - 1):
+        #                 for k in range(len(self.df[inter[1]].unique()) - 1):
+        #                     col = inter[0] + '_' + str(col_1[j]) + '_' + inter[1] + '_' + str(col_2[k])
+        #                     self.X[col] = TEMP.transpose()[j * (len(col_2) - 1) + k]
+        #         else:
+        #             if inter[0] in categorical and inter[1] not in categorical:
+        #                 cat = inter[0]
+        #                 con = inter[1]
+        #             else:
+        #                 cat = inter[1]
+        #                 con = inter[0]
+        #             col = self.df[cat].unique()
+        #             if ascending is False:
+        #                 col = col[::-1]
+        #             for j in range(len(col) - 1):
+        #                 col = cat + '_' + str(col[j]) + '_' + con
+        #                 self.X[col] = TEMP.transpose()[j]
 
         if self.fit_intercept is True:
             length = len(self.df) - len(self.X.columns.values.tolist()) - 1
@@ -163,7 +195,7 @@ class LinearRegression:
         print('Test statistic:', test_stat)
         print('Pr:', p_value)
 
-    def predict_interval(self, X, alpha, mode = 'individual'):
+    def predict_interval(self, X, alpha = 0.05, mode = 'individual'):
         if self.fit_intercept is True:
             X = np.c_[np.ones(len(X)), np.array(X)]
         else:
@@ -181,7 +213,7 @@ class LinearRegression:
                                     est + stats.t.ppf(1 - alpha / 2, len(self.df) - len(self.X.columns) - self.fit_intercept) * self.sigma * math.sqrt(std)])
         return return_list
 
-    def lack_of_fit(self, result = True):
+    def lack_of_fit(self, result = True, epsilon = 1e-10):
         lack_fit = {}
         for i in range(len(self.X)):
             temp = []
@@ -200,33 +232,36 @@ class LinearRegression:
             for i in range(len(lack_fit.get(key))):
                 pure_error += (lack_fit.get(key)[i] - mean) ** 2
 
-        test_stat = ((self.param['Res S.S.'] - pure_error) / (len(lack_fit) - len(self.X.columns) - self.fit_intercept)) / (pure_error / (len(self.df) - len(lack_fit)))
         if result is False:
             return (self.param['Res S.S.'] - pure_error) / (len(lack_fit) - len(self.X.columns) - self.fit_intercept)
 
         else:
-            print('{}\t\t\t\t\t{}'.format('Source', 'Sum of Square'))
-            print('{}:\t\t\t{}'.format('Lack of Fit', "{0:.4f}".format(self.param['Res S.S.'] - pure_error)))
-            print('{}:\t\t\t\t{}'.format('Pure Error', "{0:.4f}".format(pure_error)))
-            print('F-value:', test_stat)
-            print('Pr(Lack of Fit):', 1 - stats.f.cdf(test_stat, len(lack_fit) - len(self.X.columns) - self.fit_intercept, len(self.df) - len(lack_fit)))
-            print('')
+            if pure_error - self.param['Res S.S.'] > epsilon or pure_error - self.param['Res S.S.'] < - epsilon:
+                test_stat = ((self.param['Res S.S.'] - pure_error) / (len(lack_fit) - len(self.X.columns) - self.fit_intercept)) / (pure_error / (len(self.df) - len(lack_fit)))
+                print('{0: <10}        {1: <10}'.format('Source', 'Sum of Square'))
+                print('{0: <10}        {1: <10}'.format('Lack of Fit', "{0:.4f}".format(self.param['Res S.S.'] - pure_error)))
+                print('{0: <10}        {1: <10}'.format('Pure Error', "{0:.4f}".format(pure_error)))
+                print('F-value:', test_stat)
+                print('Pr(Lack of Fit):', 1 - stats.f.cdf(test_stat, len(lack_fit) - len(self.X.columns) - self.fit_intercept, len(self.df) - len(lack_fit)))
+                print('')
+            else:
+                print('The lack of fit cannot be measured as there are no repeated records')
 
     def summary(self):
         columns = self.X.columns.values.tolist()
-        print('{}\t\t\t{}\t\t\t{}'.format('Factor', 'Coefficient', 'Pr(|t|>0)'))
+        print('{0: <15}     {1: <15}     {2: <15}'.format('Factor', 'Coefficient', 'Pr(|t|>0)'))
         if self.fit_intercept is True:
             length = len(self.df) - len(self.X.columns.values.tolist()) - 1
-            print('{}:\t\t{}\t\t\t\t{}'.format('Intercept', "{0:.4f}".format(self.coef[0]), "{0:.4f}".format(2 * (1 - stats.t.cdf(abs(self.coef[0]) / math.sqrt(self.std[0][0]), length)))))
+            print('{0: <15}     {1: <15}     {2: <15}'.format('Intercept', "{0:.4f}".format(self.coef[0]), "{0:.4f}".format(2 * (1 - stats.t.cdf(abs(self.coef[0]) / math.sqrt(self.std[0][0]), length)))))
         else:
             length = len(self.df) - len(self.X.columns.values.tolist())
         for i in range(len(columns)):
-            print('{}:\t\t\t\t{}\t\t\t\t{}'.format(columns[i], "{0:.4f}".format(self.coef[i + self.fit_intercept]), "{0:.4f}".format(2 * (1 - stats.t.cdf(abs(self.coef[i + self.fit_intercept]) / math.sqrt(self.std[i + self.fit_intercept][i + self.fit_intercept]), length)))))
+            print('{0: <15}     {1: <15}     {2: <15}'.format(columns[i], "{0:.4f}".format(self.coef[i + self.fit_intercept]), "{0:.4f}".format(2 * (1 - stats.t.cdf(abs(self.coef[i + self.fit_intercept]) / math.sqrt(self.std[i + self.fit_intercept][i + self.fit_intercept]), length)))))
         print('------------------------------------------------------------')
-        print('{}\t\t\t\t\t{}'.format('Source', 'Sum of Square'))
-        print('{}:\t\t\t\t{}'.format('Total S.S.', "{0:.4f}".format(self.param['Total S.S.'])))
-        print('{}:\t\t\t\t{}'.format('Reg S.S.', "{0:.4f}".format(self.param['Reg S.S.'])))
-        print('{}:\t\t\t\t{}'.format('Res S.S.', "{0:.4f}".format(self.param['Res S.S.'])))
+        print('{0: <10}        {1: <10}'.format('Source', 'Sum of Square'))
+        print('{0: <10}        {1: <10}'.format('Total S.S.', "{0:.4f}".format(self.param['Total S.S.'])))
+        print('{0: <10}        {1: <10}'.format('Reg S.S.', "{0:.4f}".format(self.param['Reg S.S.'])))
+        print('{0: <10}        {1: <10}'.format('Res S.S.', "{0:.4f}".format(self.param['Res S.S.'])))
         test_stat = (self.param['Reg S.S.'] / len(self.X.columns)) / (self.param['Res S.S.'] / (len(self.df) - len(self.X.columns) - self.fit_intercept))
         print('F-value:', test_stat)
         print('Pr(F):', 1 - stats.f.cdf(test_stat, len(self.X.columns) + self.fit_intercept,
@@ -236,22 +271,22 @@ class LinearRegression:
         print('Adjusted R-squared:', (1 - (self.param['Res S.S.'] / (len(self.df) - len(self.X.columns) - self.fit_intercept)) / (self.param['Total S.S.'] / (len(self.df) - self.fit_intercept))))
         print(' ')
 
-df = pd.DataFrame({'X1' : [1, 2, 2, 3, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 9, 10, 11, 11, 11, 12],
-                   'X2' : [-1, 2, 2, 2, 2, 7, 7, 8, 8, 8, 3, 4, 4, 5, 6, 9, 10, 10, 11, 12],
-                   'X1c' : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                   'X2c' : [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-                   'y' : [-3, 7, 8, 5, 9, 20, 19, 19, 18, 20, 15, 15, 16, 18, 22, 32, 31, 34, 33, 37]})
+
+
+# df = pd.DataFrame({'X1' : [1, 2, 2, 3, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 9, 10, 11, 11, 11, 12],
+#                    'X2' : [-1, 2, 2, 2, 2, 7, 7, 8, 8, 8, 3, 4, 4, 5, 6, 9, 10, 10, 11, 12],
+#                    'X1c' : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#                    'X2c' : [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+#                    'y' : [-3, 7, 8, 5, 9, 20, 19, 19, 18, 20, 15, 15, 16, 18, 22, 32, 31, 34, 33, 37]})
 # df = pd.DataFrame({'X' : [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3],
 #                    'y' : [79.5, 82.0, 80.6, 84.9, 81.0, 81.5, 82.3, 81.4, 79.5, 83.0, 78.1, 80.2, 81.5, 83.0, 82.1]})
-model = LinearRegression(df, fit_intercept = True)
-model.fit(['X1c', 'X2c'], 'y', categorical = ['X1c', 'X2c'], ascending = False)
+# model = LinearRegression(df, fit_intercept = True)
+# model.fit(['X1c', 'X2c'], 'y', categorical = ['X1c', 'X2c'], interaction = [['X1c', 'X2c']], ascending = False)
 # model.change_sigma(1)
-# model.hypothesis_testing([{'X1c_1' : 1, 'X2c_1' : 1, 'X1c_1_X2c_1' : -1},
-#                           {'X1c_1' : -1, 'X2c_1' : 1},
-#                           {'X1c_1_X2c_1' : 1}], [0, 0, 0])
-model.summary()
+# model.summary()
+# model.hypothesis_testing([{'X1c1' : 1, 'X2c1' : 1, 'X1c1_X2c1' : 1}], [0])
 # model.lack_of_fit()
-# print(model.predict([[5, 10], [6, 9]]))
+# print(model.predict([[1, 0, 1], [0, 1, 0]]))
 
 
 
